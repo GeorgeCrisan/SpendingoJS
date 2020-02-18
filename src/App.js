906f0ca338d7225fc0f80b2626d67b317b5130ee
George Crisan <georgerdp@gmail.com>
2020-02-18 01:20:27 +0000

    feat: create Budget button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import Button from '@material-ui/core/Button';
+import ButtonSpendingo from './Components/ButtonSpendingo';
 
 function App() {
   return (
     <div className="App">
-      Hi
+      Hi Heorge
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <ButtonSpendingo>Wtf ??</ButtonSpendingo>
     </div>
   );
 }
